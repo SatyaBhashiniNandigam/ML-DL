b48406a9cbc4087ff49a93ff27602f8cd98d1d41
SatyaBhashiniNandigam <64476669+SatyaBhashiniNandigam@users.noreply.github.com>
2020-06-18 00:09:19 +0530

    Update upgrade.py

--- DL/upgrade.py
+++ DL/upgrade.py
@@ -1,6 +1,6 @@
 import os
 
-acc = os.popen("cat /var/lib/jenkins/workspace/j2/accuracy.txt")
+acc = os.popen("cat /var/lib/jenkins/workspace/job2/accuracy.txt")
 acc1 = acc.read()
 print(acc1)
 acc2 = acc1.rstrip()
@@ -9,7 +9,7 @@ acc3 = float(acc2)
 
 
 if acc3<80:
-    x = os.popen("cat /var/lib/jenkins/workspace/j2/train.py | grep model.add | wc -l")
+    x = os.popen("cat /var/lib/jenkins/workspace/job2/train.py | grep model.add | wc -l")
     x1 = x.read()
     x2 = x1.rstrip()
     x3 = int(x2)
@@ -23,9 +23,9 @@ if acc3<80:
     else:
         print("sorry:( try again")
         exit()
-    os.system("sed -i '/softmax/ i {}' /var/lib/jenkins/workspace/j2/train.py".format(y))
-    os.system("curl -u root:redhat http://192.168.56.102:8080/view/Ml%20+%20Jenkins/job/j2/build?token=satvi")
-    acc = os.popen("cat /var/lib/jenkins/workspace/j2/accuracy.txt")
+    os.system("sed -i '/softmax/ i {}' /var/lib/jenkins/workspace/job2/train.py".format(y))
+    os.system("curl -u root:redhat http://192.168.56.102:8080/view/Ml%20+%20Jenkins/job/job2/build?token=satya")
+    acc = os.popen("cat /var/lib/jenkins/workspace/job2/accuracy.txt")
     acc1 = acc.read()
     print(acc1)
     acc2 = acc1.rstrip()
